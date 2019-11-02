69cc673e2a1803dc85003974e203b369b805efe2
Patrick Lee <cluster000@gmail.com>
2019-11-02 16:35:04 +0800

    Initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import App from './App';
+import CommentBox from './components/CommentBox';
+import CommentList from './components/CommentList';
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
+
+it('shows a comment box', () => {
+  expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
+it('shows a comment list', () => {
+  expect(wrapped.find(CommentList).length).toEqual(1);
 });
